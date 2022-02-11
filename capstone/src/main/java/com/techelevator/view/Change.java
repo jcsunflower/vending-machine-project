a5fb491fd796d8eb275fd5f90e6ce0f8833c17c5
Ahmet Yildirim <yldrmao@gmail.com>
2022-02-10 23:16:13 -0500

    Ahmet

---
+++ capstone/src/main/java/com/techelevator/view/Change.java
@@ -0,0 +1,15 @@
+package com.techelevator.view;
+
+import java.math.BigDecimal;
+
+public class Change {
+
+    private BigDecimal nickel = new BigDecimal(0.05);
+    private BigDecimal dime = new BigDecimal(0.10);
+    private BigDecimal quarter = new BigDecimal(0.25);
+
+
+
+
+
+}
