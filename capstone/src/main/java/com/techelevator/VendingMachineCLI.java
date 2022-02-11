3898411c6768c04b3b7986d31aacb7883c9f03ef
Ahmet Yildirim <yldrmao@gmail.com>
2022-02-10 22:03:50 -0500

    Ahmet

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,33 +2,54 @@ package com.techelevator;
 
 import com.techelevator.view.Menu;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class VendingMachineCLI {
 
-	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
-	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+    private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+    private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+    private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE};
+    private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
+    private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Purchase";
+    private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
+    private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
+    private Menu menu;
 
-	private Menu menu;
+    public VendingMachineCLI(Menu menu) {
+        this.menu = menu;
+    }
 
-	public VendingMachineCLI(Menu menu) {
-		this.menu = menu;
-	}
+    public void run() {
+        while (true) {
+            String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
-	public void run() {
-		while (true) {
-			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+            if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+                File items = new File("C:\\Users\\Student\\Desktop\\Vending machine\\capstone-1-team-9\\capstone\\vendingmachine.csv");
+                Scanner display = null;
+                try {
+                    display = new Scanner(items);
+                    while (display.hasNextLine()) {
+                        System.out.println(display.nextLine());
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
 
-			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
-			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				// do purchase
-			}
-		}
-	}
+                while (display.hasNextLine()) {
+                    System.out.println(display.nextLine());
+                }
+            } else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+                String choicePurchase = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		Menu menu = new Menu(System.in, System.out);
-		VendingMachineCLI cli = new VendingMachineCLI(menu);
-		cli.run();
-	}
+    public static void main(String[] args) {
+        Menu menu = new Menu(System.in, System.out);
+        VendingMachineCLI cli = new VendingMachineCLI(menu);
+        cli.run();
+    }
 }
